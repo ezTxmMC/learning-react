42967a06ee584e868c61746839de37cddef54bbe
ezTxmMC <eztxmmc@hotmail.com>
2025-02-13 11:30:34 +0100

    added products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,26 @@
 import Header from "../components/Header";
 
 export default function Products() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div id="products">
+        <div class="product">
+          <button onClick={() => window.open("/product/product-1", "_self")}>
+            Product 1
+          </button>
+        </div>
+        <div id="product">
+          <button onClick={() => window.open("/product/product-2", "_self")}>
+            Product 2
+          </button>
+        </div>
+        <div id="product">
+          <button onClick={() => window.open("/product/product-3", "_self")}>
+            Product 3
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
